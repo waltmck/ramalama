4c8d41cef5a4190adb744d12db11bd465d622af9
Walter McKelvie <walt@mckelvie.org>
2025-09-17 16:27:13 -0400

    add pyyaml

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
                 dependencies = [ (llama-cpp.override llamaCppOverrides) ];
                 nativeBuildInputs =
                   (with pkgs; [ codespell shellcheck isort bats jq apacheHttpd ]) ++
-                  (with pkgs.python3Packages; [ flake8 black pytest ]);
+                  (with pkgs.python3Packages; [ flake8 black pytest pyyaml ]);
                 pyproject = true;
                 build-system = with pkgs.python3Packages; [ setuptools ];
               } // ramalamaOverrides)
